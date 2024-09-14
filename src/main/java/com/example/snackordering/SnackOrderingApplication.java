cb4c545c3a3352de42b72d49bd50373c6f91e224
PhCh2k3 <phongdcse171753@fpt.edu.vn>
2024-09-15 06:46:54 +0700

    first commit

---
+++ src/main/java/com/example/snackordering/SnackOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.example.snackordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackOrderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnackOrderingApplication.class, args);
+    }
+
+}
